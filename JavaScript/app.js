9dda06e81f3f8b1160fa33fbac275c342e7b3a07
CarnageOG <egetashviligiorgi9@gmail.com>
2025-03-21 18:22:55 +0400

    made inforamtion about me section

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -8,18 +8,18 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 5000);
 });
 ///.2.////////////////////////////////////////////////////////////////////
-        // function fillProgressBars() {
-        //     const progressBars = document.querySelectorAll('.progress');
-        //     progressBars.forEach(bar => {
-        //         const rect = bar.getBoundingClientRect();
-        //         const windowHeight = window.innerHeight;
-        //         if (rect.top < windowHeight - 50 && !bar.classList.contains('filled')) {
-        //             bar.style.width = bar.getAttribute('data-percent') + '%';
-        //             bar.classList.add('filled');
-        //         }
-        //     });
-        // }
+        function fillProgressBars() {
+            const progressBars = document.querySelectorAll('.progress');
+            progressBars.forEach(bar => {
+                const rect = bar.getBoundingClientRect();
+                const windowHeight = window.innerHeight;
+                if (rect.top < windowHeight - 50 && !bar.classList.contains('filled')) {
+                    bar.style.width = bar.getAttribute('data-percent') + '%';
+                    bar.classList.add('filled');
+                }
+            });
+        }
 
-        // window.addEventListener('scroll', fillProgressBars);
-        // window.addEventListener('load', fillProgressBars);;
+        window.addEventListener('scroll', fillProgressBars);
+        window.addEventListener('load', fillProgressBars);;
 //////////////////////////////////////////////////////////////////////////
