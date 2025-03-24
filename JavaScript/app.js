8e1d2910408e813b761bb0dbcc5b9e59c5535d25
CarnageOG <egetashviligiorgi9@gmail.com>
2025-03-24 14:57:17 +0400

    added contact container

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,25 +1,26 @@
-///.1.////////////////////////////////////////////////////////////////////
-document.addEventListener("DOMContentLoaded", function() {
-    let images = ["images/1.PNG", "images/2.PNG", "images/3.PNG"];
-    let index = 0;
-    setInterval(() => {
-        index = (index + 1) % images.length;
-        document.querySelector(".background-img").src = images[index];
-    }, 5000);
-});
-///.2.////////////////////////////////////////////////////////////////////
-        function fillProgressBars() {
-            const progressBars = document.querySelectorAll('.progress');
-            progressBars.forEach(bar => {
-                const rect = bar.getBoundingClientRect();
-                const windowHeight = window.innerHeight;
-                if (rect.top < windowHeight - 50 && !bar.classList.contains('filled')) {
-                    bar.style.width = bar.getAttribute('data-percent') + '%';
-                    bar.classList.add('filled');
-                }
-            });
-        }
+// ///.1.////////////////////////////////////////////////////////////////////
+// document.addEventListener("DOMContentLoaded", function() {
+//     let images = ["images/1.PNG", "images/2.PNG", "images/3.PNG"];
+//     let index = 0;
+//     setInterval(() => {
+//         index = (index + 1) % images.length;
+//         document.querySelector(".background-img").src = images[index];
+//     }, 5000);
+// });
+// ///.2.////////////////////////////////////////////////////////////////////
+//         function fillProgressBars() {
+//             const progressBars = document.querySelectorAll('.progress');
+//             progressBars.forEach(bar => {
+//                 const rect = bar.getBoundingClientRect();
+//                 const windowHeight = window.innerHeight;
+//                 if (rect.top < windowHeight - 50 && !bar.classList.contains('filled')) {
+//                     bar.style.width = bar.getAttribute('data-percent') + '%';
+//                     bar.classList.add('filled');
+//                 }
+//             });
+//         }
 
-        window.addEventListener('scroll', fillProgressBars);
-        window.addEventListener('load', fillProgressBars);;
-//////////////////////////////////////////////////////////////////////////
+//         window.addEventListener('scroll', fillProgressBars);
+//         window.addEventListener('load', fillProgressBars);;
+// //////////////////////////////////////////////////////////////////////////
+
