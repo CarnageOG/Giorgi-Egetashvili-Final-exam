567bd78bc1a8017baddfdee1d768f80389f5851f
CarnageOG <egetashviligiorgi9@gmail.com>
2025-03-26 20:08:20 +0400

    made recommendation secetion

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -23,3 +23,45 @@ document.addEventListener("DOMContentLoaded", function() {
         window.addEventListener('scroll', fillProgressBars);
         window.addEventListener('load', fillProgressBars);;
 //////////////////////////////////////////////////////////////////////////
+const slides = [
+    {
+        text: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore..",
+        image: "images/mau.png",
+        name: "Mau Thomas",
+        profession: "Graphic Designer"
+    },
+    {
+        text: "if you want your son high level wrestling send him 2-3 years to dagestan and forget, six months one time you can call him.",
+        image: "images/islam.PNG",
+        name: "Islam Makhachev",
+        profession: "MMA Fighter"
+    },
+    {
+        text: "The trick to catching a liar, my dear friend, is not to confront them directly. No, no. Instead, you let them believe they have fooled you… until they are too comfortable to maintain the illusion",
+        image: "images/hans.PNG",
+        name: "Hans Landa",
+        profession: "Standartenführer"
+    }
+];
+
+let currentSlide = 0;
+
+function updateSlide() {
+    document.querySelector(".recommendation-text").innerText = slides[currentSlide].text;
+    document.querySelector(".person").src = slides[currentSlide].image;
+    document.querySelector(".recommendator-names").innerText = slides[currentSlide].name;
+    document.querySelector(".profesions").innerText = slides[currentSlide].profession;
+    
+    document.querySelectorAll(".button").forEach((button, index) => {
+        button.style.borderColor = index === currentSlide ? "#E93656" : "white";
+    });
+}
+
+document.querySelectorAll(".button").forEach((button, index) => {
+    button.addEventListener("click", () => {
+        currentSlide = index;
+        updateSlide();
+    });
+});
+
+updateSlide();
